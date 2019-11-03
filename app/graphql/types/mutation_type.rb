24eff09ba9ab259f46f9a271de6678e668302b0c
Emmanuel Aboderin <emmanuela@buycoins.africa>
2019-11-03 11:56:45 +0100

    add mutation for subscribing users

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,24 @@
 module Types
   class MutationType < Types::BaseObject
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+    # field :test_field, String, null: false,
+    #   description: "An example field added by the generator"
+    # def test_field
+    #   "Hello World"
+    # end
+
+    field :weather_updates_subscription, String, null: false do
+      argument :email, String, required: true
+      argument :city, String, required: true
     end
+
+    def weather_updates_subscription(email:, city:)
+      subscription = Subscription.new
+      subscription.email = email
+      subscription.city = city
+      result = subscription.save
+      result ? "Your subscription has been successfully made" : "Error! Subscription could not be made"
+    end
+
   end
 end
