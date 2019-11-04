ea1f8577013e08a58981f84b3b8e80ad7a518b3e
Emmanuel Aboderin <emmanuela@buycoins.africa>
2019-11-04 01:56:49 +0100

    add clockwork to schedule daily email updates

--- app/interactors/add_subcription.rb
+++ app/interactors/add_subcription.rb
@@ -6,21 +6,15 @@ class AddSubcription
     # check that city is among the list
     response = ValidateCity.call(city: context.city)
     # fail if city is nil
-    if response.city_available == nil
-      context.fail!
-    end
+    context.fail! if !response.city_available
 
     check_records = Subscription.find_by(email: context.email, city: context.city)
-    if check_records
-      context.fail!
-    end
+    context.fail! if check_records
 
-    subscription = Subscription.new(email: context.email, city: context.city)
-    #Checks if record exists
-    if !subscription.new_record?
-      context.fail!
-    end
-
-    context.subscription_made = subscription.save
+    context.subscription_made = Subscription.create!(email: context.email, city: context.city)
   end
 end
+
+# NOTES
+# check the shebang(!) usecases
+# read up about Ruby truthy values
--- config/clock.rb
+++ config/clock.rb
@@ -1,10 +1,13 @@
 require "clockwork"
 require "active_support/time"
+require_relative "../config/boot"
+require_relative "../config/environment"
 
 module Clockwork
-    # handler do |job|
-    #     puts "Running job"
-    # end
-    
-    every(1.minutes, 'puts "Running job"')
+
+    every(1.day, 'weather_update_worker.worker', :at => '06:30'){
+        Subscription.all.each do |s|
+            WeatherUpdateWorker.perform_async(s.city, s.email)
+        end
+    }
 end
