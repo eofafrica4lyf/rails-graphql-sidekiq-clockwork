5c317bf11b86cc2bd958c2b60b6942c493e90ebe
Emmanuel Aboderin <emmanuela@buycoins.africa>
2019-11-05 10:26:04 +0100

    switching to sendgrid

--- config/clock.rb
+++ config/clock.rb
@@ -5,7 +5,7 @@ require_relative "../config/environment"
 
 module Clockwork
 
-    every(1.day, 'weather_update_worker.worker', :at => '06:30'){
+    every(1.day, 'weather_update_worker.worker', :at => '11:00'){
         Subscription.all.each do |s|
             WeatherUpdateWorker.perform_async(s.city, s.email)
         end
